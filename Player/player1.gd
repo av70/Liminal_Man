e3f8784eec392366fc1807007b7d285450b6c44d
Alexander Vartan <140988442+av70@users.noreply.github.com>
2023-09-21 00:27:14 -0700

    audio queue, brocken character states

--- Player/player1.gd
+++ Player/player1.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
+# to do : sometimes you are unable to jump when you should while descending 
+# staircases
 
-#player nodes
 @onready var player = $"."
 @onready var neck_pivot = $NeckPivot
 @onready var camera = $NeckPivot/Camera
@@ -9,23 +10,25 @@ extends CharacterBody3D
 @onready var tip_toe_collision = $tip_toe_collision
 @onready var standing_collision = $standing_collision
 @onready var floor_raycast = $FloorRaycast
-
+@onready var audio_queue = $NeckPivot/AudioQueue
 
 
 @onready var head_raycast = $HeadRaycast
 
-#input vairbels
 @export var current_speed: float = 5.0
 @export var move_lerp_speed: int = 10
+@export var char_state: int = 6
+
+# [0 = idle, 1 = walk, 2 = sneak, 3 = run, 4 = jump, 5 = lunge, 6 = fall]
 
 var mouse_sense: float = 0.4
 var direction: Vector3 = Vector3.ZERO
 var grounded: bool = false
 
 const walk_speed: int = 5
-const run_speed: int  = 7
-const tip_toe_speed: int  = 3
-const lunge_speed: int  = 10
+const run_speed: int = 7
+const tip_toe_speed: int = 3
+const lunge_speed: int = 10
 const jump_velocity: int  = 5
 var tip_toe_depth: float = -0.4 # amount the camera lowers relative to regular
 
@@ -34,18 +37,17 @@ var tip_toe_depth: float = -0.4 # amount the camera lowers relative to regular
 	# {w, a, s, d} -> move [duh]
 
 	# spacebar-> jump [duuurh]
-	# 	[normal hitbox, no extra forward velocity, 1.25x upward velocity than normal jump]
+		#[normal hitbox, no extra forward velocity, 1.25x upward velocity than normal jump]
 	
 	# move + shift -> run 
 
 	# c -> tip toe
-	# 	[hitbox shortened, slow speed]
+		# [hitbox shortened, slow speed]
 
 	# shift + spacebar -> lunge
-	# 	[shortened hitbox, extra forward velocity, .8x upward velocity than normal jump]
+		# [shortened hitbox, extra forward velocity, .8x upward velocity than normal jump]
 
 	# [secret four digit code] -> zumbo sauce mode
-
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -58,8 +60,49 @@ func _input(event):
 		neck_pivot.rotate_x(deg_to_rad(-event.relative.y*mouse_sense))
 		neck_pivot.rotation.x = clamp(neck_pivot.rotation.x,(deg_to_rad(-89)),(deg_to_rad(89)))
 
-func crouch(boolean: bool,delta):
-	
+func check_state() -> int:
+	if grounded: # [ground]
+		if velocity.x+velocity.z >= 0.4 or velocity.x+velocity.z <= -0.4:
+			if current_speed >= lunge_speed:
+				print('run')
+				return 2 # run
+			elif current_speed >= walk_speed:
+				print('walk')
+				return 1 # walk
+		else: 
+			print('idle')
+			return 0 # idle
+			
+	if velocity.y < -1: # [air]
+		if current_speed >= run_speed:
+			print('jump')
+			return 4 # jump
+		elif current_speed >= lunge_speed:
+			print('lunge')
+			return 5 # lunge
+	else:
+		print('fall')
+		return 6 # fall
+	return 0 # zumbo sauce mode !
+
+func set_speed(speed: float) -> void:
+	current_speed = lerp(current_speed,speed,0.5)
+
+func set_state() -> void:
+	var new_state = check_state()
+	if char_state != check_state(): 
+		if new_state > 0 and new_state < 4:
+			pass
+		if new_state >= 3 and char_state <= 4:
+			print('no footstep')
+		elif new_state < 4 and char_state > 3:
+			if floor_raycast.is_colliding():
+				audio_queue.play_audio()
+		print(new_state,char_state)
+		char_state = check_state()
+		
+
+func crouch(boolean: bool,delta) -> void:
 	if boolean:
 		neck_pivot.position.y = lerp(neck_pivot.position.y,1.2+tip_toe_depth,delta*.4*move_lerp_speed)
 		tip_toe_collision.disabled = false
@@ -68,9 +111,7 @@ func crouch(boolean: bool,delta):
 		neck_pivot.position.y = lerp(neck_pivot.position.y,1+-tip_toe_depth,delta*.4*move_lerp_speed)
 		tip_toe_collision.disabled = true
 		standing_collision.disabled = false
-		
-func set_speed(speed: float):
-	current_speed = lerp(current_speed,speed,0.5)
+
 
 func _physics_process(delta):
 	if not grounded:
@@ -89,7 +130,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
 	
-	if Input.is_action_pressed("tip_toe"):
+	if Input.is_action_pressed("sneak"):
 		crouch(true,delta)
 		if grounded:
 			set_speed(tip_toe_speed)
@@ -101,14 +142,14 @@ func _physics_process(delta):
 		elif velocity.y >= 1:
 			set_speed(lunge_speed)
 			crouch(true,delta)
-	
+		
 	elif grounded:
 		set_speed(walk_speed)
 		crouch(false,delta)
-
+	
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"): 
-		if grounded:
+		if is_on_floor() or grounded:
 			if Input.is_action_pressed("run"):
 				velocity.y = jump_velocity*.8
 			else:
@@ -116,8 +157,9 @@ func _physics_process(delta):
 	
 	if floor_raycast.is_colliding(): # how player y value is treated
 		var floor_target: Vector3 = floor_raycast.get_collision_point()
-
-		if velocity.x+velocity.z >= 0.1 or velocity.x+velocity.z <= -0.1:
+		if velocity.y < 0:
+			velocity.y = -current_speed/2 # prevents phasing through floor
+		if velocity.x+velocity.z >= 0.4 or velocity.x+velocity.z <= -0.4:
 			# i know this isn't the best solution but i'm too busy doin' yo mom
 			position.y  = move_toward(position.y,floor_target.y,current_speed*delta)
 		else:
@@ -126,5 +168,8 @@ func _physics_process(delta):
 		grounded = true
 	else:
 		grounded = false
+	
+	set_state()
+
 
 	move_and_slide()
---
+++ entities/office/door_data.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name DoorData
+
+@export var close_animation: Animation
+@export var open_animation: Animation
+@export var fast_open_animation: Animation
